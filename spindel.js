a314bf58b1cd83f7668b671ceeaf7dde42b756c5
Tbar <atomizer3@gmail.com>
2025-02-04 22:37:18 +0100

    Zmiana hosta+dodanie cors,mysql,mysql2

--- spindel.js
+++ spindel.js
@@ -1,27 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+app.use(cors());
 
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '192.168.0.109',
   user: 'spindel',
   password: 'q1w2e3Q!W@E#',
   database: 'SPINDEL_DB'
 })
 
-connection.connect()
+//connection.connect()
 
-connection.query('SELECT ID, TEMP_VALUE from SPINDEL_DB.BREW_TEMP', (err, rows, fields) => {
-  if (err) throw err
-  console.log('Query result: ', rows)
-  console.log('row', rows[0].ID+' '+rows[0].TEMP_VALUE)
-})
+//setInterval(()=>{
+//	connection.query('SELECT ID, TEMP_VALUE from SPINDEL_DB.BREW_TEMP', (err, rows, fields) => {
+//	  if (err) throw err
+//	  console.log('Query result: ', rows)
+//	  console.log('row', rows[0].ID+' '+rows[0].TEMP_VALUE)
+//	})
+//},3000)
 
-connection.end()
+
+//connection.end()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log('Dupa')
+  res.send({resp:'Hello World!'})
 })
 
 app.listen(port, () => {
